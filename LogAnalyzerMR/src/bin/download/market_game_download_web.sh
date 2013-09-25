46c0c1fba3a5c662860385201d723050c6592b24
gadfly2008 <gadfly2008@gmail.com>
2013-09-25 11:23:55 +0800

    重构

--- LogAnalyzerMR/src/bin/download/market_game_download_web.sh
+++ LogAnalyzerMR/src/bin/download/market_game_download_web.sh
@@ -37,26 +37,126 @@ sudo -u hdfs hive -e "
         from web814 
         where gameId>0
         group by from_unixtime(SERVER_TIME,'yyyy-MM-dd'),unix_timestamp(from_unixtime(SERVER_TIME,'yyyy-MM-dd'),'yyyy-MM-dd') ,gameId;
+        
+    
+    drop table market_downlog_web;
+    Create Table market_downlog_web as  select a.day as day ,a.time as time,sum(a.total) as total from market_game_download_web a group by a.day,a.time;
+    
+    
+    drop table market_game_download_click_stat_tmp;
+    create table if not exists market_game_download_click_stat_tmp (
+        day string,
+        apkid int,
+        total int
+    )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+
+    insert overwrite table market_game_download_click_stat_tmp 
+        select case when CLIENT_TIME>SERVER_TIME*1000 or SERVER_TIME>(CLIENT_TIME/1000)+864000 then from_unixtime(SERVER_TIME,'yyyy-MM-dd') else from_unixtime(floor(CLIENT_TIME/1000),'yyyy-MM-dd') end as day,
+            APK_ID,count(*) as total 
+        from sdk200001 
+        where APK_ID<10000000000 and APK_ID>0 and VERSION_CODE<10000000000 and VERSION_CODE>0
+        group by case when CLIENT_TIME > SERVER_TIME*1000 or SERVER_TIME>(CLIENT_TIME/1000)+864000 then from_unixtime(SERVER_TIME,'yyyy-MM-dd') else from_unixtime(floor(CLIENT_TIME/1000),'yyyy-MM-dd') end,APK_ID;
+    
+    drop    table market_download_click_stat ;
+    Create Table market_download_click_stat as  select a.day as day ,sum(a.total) as total from market_game_download_click_stat_tmp a group by a.day;
+    
+    drop   table market_downlog_all;
+    create table if not exists market_downlog_all (
+         day string,
+         time int,
+         web_count int,
+         client_count int,
+         total int
+     )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+    
+    insert overwrite table market_downlog_all 
+    SELECT a.day,a.time,a.total, b.total,a.total+b.total FROM market_downlog_web a JOIN market_download_click_stat b ON (a.day = b.day)   order by a.day desc;
+    
+    drop    table market_download_click_stat ;
+    
+    
+    drop table market_all_download_stat;
+    create table if not exists market_all_download_stat (
+         day string,
+         time int,
+         gameId int,
+         web_count int,
+         client_count int,
+         total int
+     )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+    
+    
+    insert overwrite table market_all_download_stat 
+    SELECT a.*, b.total , a.total+b.total FROM market_game_download_web a JOIN market_game_download_click_stat_tmp b ON (a.day = b.day and a.gameId=b.apkid) order by a.day desc;
+    
+    
     
  "
  
  
  mysql -h10.1.1.16 -ustatsdkuser -pstatsdkuser2111579711 -D stat_sdk <<EOF
 
-	DROP TABLE IF EXISTS market_game_download_web;
-	CREATE TABLE market_game_download_web (
-		  day varchar(255) NOT NULL,
-		  time int(10) NOT NULL,
-		  gameId int(10) NOT NULL,
-		  total int(10) NOT NULL,
-		  KEY index_gameId (gameId)
-	);
-
+    DROP TABLE IF EXISTS market_game_download_web;
+    CREATE TABLE market_game_download_web (
+         day varchar(255) NOT NULL,
+         time int(10) NOT NULL,
+         gameId int(10) NOT NULL,
+         total int(10) NOT NULL,
+         KEY index_gameId (gameId)
+    );
+    
+    DROP TABLE IF EXISTS market_all_download_stat;
+    CREATE TABLE market_all_download_stat (
+         day varchar(255) NOT NULL,
+         time int(10) NOT NULL,
+         gameId int(10) NOT NULL,
+         web_count int(10) NOT NULL,
+         client_count int(10) NOT NULL,
+         total int(10) NOT NULL,
+         KEY index_gameId (gameId),
+	     KEY index_web_count (web_count),
+	     KEY index_client_count (client_count),
+	     KEY index_total (total)
+    );
+    
+    DROP TABLE IF EXISTS market_downlog_web;
+    CREATE TABLE market_downlog_web (
+         day varchar(255) NOT NULL,
+         time int(10) NOT NULL,
+         total int(10) NOT NULL
+    );
+    
+    DROP TABLE IF EXISTS market_downlog_all;
+    CREATE TABLE market_downlog_all (
+         day varchar(255) NOT NULL,
+         time int(10) NOT NULL,
+         web_count int(10) NOT NULL,
+         client_count int(10) NOT NULL,
+         total int(10) NOT NULL
+    );
+    
 EOF
 
  sudo -u hdfs  sqoop export --connect jdbc:mysql://10.1.1.16:3306/stat_sdk --username statsdkuser --password statsdkuser2111579711 --table market_game_download_web --export-dir /user/hive/warehouse/market_game_download_web --input-fields-terminated-by '\t' --input-null-string "\\\\N" --input-null-non-string "\\\\N"
+ sudo -u hdfs  sqoop export --connect jdbc:mysql://10.1.1.16:3306/stat_sdk --username statsdkuser --password statsdkuser2111579711 --table market_all_download_stat --export-dir /user/hive/warehouse/market_all_download_stat --input-fields-terminated-by '\t' --input-null-string "\\\\N" --input-null-non-string "\\\\N"
+ sudo -u hdfs  sqoop export --connect jdbc:mysql://10.1.1.16:3306/stat_sdk --username statsdkuser --password statsdkuser2111579711 --table market_downlog_web --export-dir /user/hive/warehouse/market_downlog_web --input-fields-terminated-by '\001' --input-null-string "\\\\N" --input-null-non-string "\\\\N"
+ sudo -u hdfs  sqoop export --connect jdbc:mysql://10.1.1.16:3306/stat_sdk --username statsdkuser --password statsdkuser2111579711 --table market_downlog_all --export-dir /user/hive/warehouse/market_downlog_all --input-fields-terminated-by '\t' --input-null-string "\\\\N" --input-null-non-string "\\\\N"
 
 mysql -h10.1.1.16 -ustatsdkuser -pstatsdkuser2111579711 -D stat_sdk -e "ALTER TABLE  market_game_download_web  ADD id INT( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY   FIRST ;"
+mysql -h10.1.1.16 -ustatsdkuser -pstatsdkuser2111579711 -D stat_sdk -e "ALTER TABLE  market_all_download_stat  ADD id INT( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY   FIRST ;"
+mysql -h10.1.1.16 -ustatsdkuser -pstatsdkuser2111579711 -D stat_sdk -e "ALTER TABLE  market_downlog_web  ADD id INT( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY   FIRST ;"
+mysql -h10.1.1.16 -ustatsdkuser -pstatsdkuser2111579711 -D stat_sdk -e "ALTER TABLE  market_downlog_all  ADD id INT( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY   FIRST ;"
+
+
 
  
  
---
+++ LogAnalyzerMR/src/bin/download/market_mobile_download_stat.sh
@@ -0,0 +1,422 @@
+#!/bin/bash
+
+sudo -u hdfs hive -e "
+    create EXTERNAL table if not exists sdk200001 (
+        SERVER_TIME bigint,
+        CLIENT_IP string,
+        CLIENT_AREA string,
+        EVENT_CLASS_ID string,
+        CLIENT_TIME bigint,
+        EVENT_TAG string,
+        CELL_PHONE_BRAND string,
+        CELL_PHONE_MODEL string,
+        CELL_PHONE_CPU string,
+        CELL_PHONE_DENSITY string,
+        CELL_PHONE_SCREEN_WIDTH bigint,
+        CELL_PHONE_SCREEN_HEIGHT bigint,
+        CELL_PHONE_NETWORK string,
+        CELL_PHONE_SYSTEM_VERSION string,
+        MUZHIWAN_VERSION string,
+        CELL_PHONE_FIRMWARE string,
+        CELL_PHONE_DEVICE_ID string,
+        TITLE string,
+        PACKAGE_NAME string,
+        VERSION string,
+        VERSION_CODE bigint,
+        APK_LINK string,
+        APK_ID bigint,
+        APK_BAIDU_LINK string,
+        APK_SIZE bigint,
+        FLAG_GOOGLE_PLAY bigint,
+        FLAG_LOGIN_ACCOUNT bigint,
+        OPERATION_TAG string,
+        CHANNEL string,
+        APK_SOURCE string,
+        CATEGORY string
+    )
+    row format delimited
+    fields terminated by '\\001'
+    stored as textfile
+    location '/apilogs/src/200001/';
+  
+    drop table market_game_download_click_stat_tmp;
+    create table if not exists market_game_download_click_stat_tmp (
+        day string,
+        apkid int,
+        total int
+    )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+
+    insert overwrite table market_game_download_click_stat_tmp 
+        select case when CLIENT_TIME>SERVER_TIME*1000 or SERVER_TIME>(CLIENT_TIME/1000)+864000 then from_unixtime(SERVER_TIME,'yyyy-MM-dd') else from_unixtime(floor(CLIENT_TIME/1000),'yyyy-MM-dd') end as day,
+            APK_ID,count(*) as total 
+        from sdk200001 
+        where VERSION_CODE<10000000000
+        group by case when CLIENT_TIME > SERVER_TIME*1000 or SERVER_TIME>(CLIENT_TIME/1000)+864000 then from_unixtime(SERVER_TIME,'yyyy-MM-dd') else from_unixtime(floor(CLIENT_TIME/1000),'yyyy-MM-dd') end,APK_ID;
+    
+    drop table market_game_download_click_stat;
+    create table if not exists market_game_download_click_stat (
+         day string,
+         time int,
+         apkid int,
+         total int
+     )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+    
+    insert overwrite table market_game_download_click_stat 
+     SELECT day,unix_timestamp(day,'yyyy-MM-dd')  as time,apkid,total 
+     FROM market_game_download_click_stat_tmp 
+     where total>0 and apkid<10000000000 and apkid>0
+     order by total desc;
+    
+    drop table market_game_download_click_stat_tmp;
+    
+    
+    
+    
+    
+    create EXTERNAL table if not exists sdk200003 (
+        SERVER_TIME bigint,
+        CLIENT_IP string,
+        CLIENT_AREA string,
+        EVENT_CLASS_ID string,
+        CLIENT_TIME bigint,
+        EVENT_TAG string,
+        CELL_PHONE_BRAND string,
+        CELL_PHONE_MODEL string,
+        CELL_PHONE_CPU string,
+        CELL_PHONE_DENSITY string,
+        CELL_PHONE_SCREEN_WIDTH bigint,
+        CELL_PHONE_SCREEN_HEIGHT bigint,
+        CELL_PHONE_NETWORK string,
+        CELL_PHONE_SYSTEM_VERSION string,
+        MUZHIWAN_VERSION string,
+        CELL_PHONE_FIRMWARE string,
+        CELL_PHONE_DEVICE_ID string,
+        TITLE string,
+        PACKAGE_NAME string,
+        VERSION string,
+        VERSION_CODE bigint,
+        APK_LINK string,
+        APK_ID bigint,
+        APK_BAIDU_LINK string,
+        APK_SIZE bigint,
+        FLAG_GOOGLE_PLAY bigint,
+        FLAG_LOGIN_ACCOUNT bigint,
+        OPERATION_TAG string,
+        CHANNEL string,
+        APK_SOURCE string,
+        CATEGORY string,
+        COST_TIME bigint,
+        SERVER_IP string,
+        PROGRESS bigint,
+        REALURL string
+    )
+    row format delimited
+    fields terminated by '\\001'
+    stored as textfile
+    location '/apilogs/src/200003/';
+  
+    drop table market_game_download_success_stat_tmp;
+    
+    create table if not exists market_game_download_success_stat_tmp (
+        day string,
+        apkid int,
+        total int
+    )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+
+    insert overwrite table market_game_download_success_stat_tmp 
+        select case when CLIENT_TIME>SERVER_TIME*1000 or SERVER_TIME>(CLIENT_TIME/1000)+864000 then from_unixtime(SERVER_TIME,'yyyy-MM-dd') else from_unixtime(floor(CLIENT_TIME/1000),'yyyy-MM-dd') end as day,
+            APK_ID,count(*) as total 
+        from sdk200003 
+        where VERSION_CODE<10000000000
+        group by case when CLIENT_TIME > SERVER_TIME*1000 or SERVER_TIME>(CLIENT_TIME/1000)+864000 then from_unixtime(SERVER_TIME,'yyyy-MM-dd') else from_unixtime(floor(CLIENT_TIME/1000),'yyyy-MM-dd') end,APK_ID;
+    
+    drop table market_game_download_success_stat;
+    create table if not exists market_game_download_success_stat (
+         day string,
+         time int,
+         apkid int,
+         total int
+     )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+    
+    insert overwrite table market_game_download_success_stat 
+     SELECT day,unix_timestamp(day,'yyyy-MM-dd')  as time,apkid,total 
+     FROM market_game_download_success_stat_tmp 
+     where total>0 and apkid<10000000000 and apkid>0
+     order by total desc;
+    
+    drop table market_game_download_success_stat_tmp;
+    
+    
+    
+    
+     create EXTERNAL table if not exists sdk200002 (
+        SERVER_TIME bigint,
+        CLIENT_IP string,
+        CLIENT_AREA string,
+        EVENT_CLASS_ID string,
+        CLIENT_TIME bigint,
+        EVENT_TAG string,
+        CELL_PHONE_BRAND string,
+        CELL_PHONE_MODEL string,
+        CELL_PHONE_CPU string,
+        CELL_PHONE_DENSITY string,
+        CELL_PHONE_SCREEN_WIDTH bigint,
+        CELL_PHONE_SCREEN_HEIGHT bigint,
+        CELL_PHONE_NETWORK string,
+        CELL_PHONE_SYSTEM_VERSION string,
+        MUZHIWAN_VERSION string,
+        CELL_PHONE_FIRMWARE string,
+        CELL_PHONE_DEVICE_ID string,
+        TITLE string,
+        PACKAGE_NAME string,
+        VERSION string,
+        VERSION_CODE bigint,
+        APK_LINK string,
+        APK_ID bigint,
+        APK_BAIDU_LINK string,
+        APK_SIZE bigint,
+        FLAG_GOOGLE_PLAY bigint,
+        FLAG_LOGIN_ACCOUNT bigint,
+        OPERATION_TAG string,
+        CHANNEL string,
+        errorcode int,
+        errormsg string,
+        responsecode int,
+        savepath string,
+        url string,
+        sdcardavaliablesize int,
+        sdcardtotalsize int,
+        sdcardmounted int,
+        serverip string,
+        contact string
+    )
+    row format delimited
+    fields terminated by '\\001'
+    stored as textfile
+    location '/apilogs/src/200002/';
+  
+    drop table market_game_download_error_stat_tmp;
+    create table if not exists market_game_download_error_stat_tmp (
+        day string,
+        apkid int,
+        total int
+    )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+
+    insert overwrite table market_game_download_error_stat_tmp 
+        select case when CLIENT_TIME>SERVER_TIME*1000 or SERVER_TIME>(CLIENT_TIME/1000)+864000 then from_unixtime(SERVER_TIME,'yyyy-MM-dd') else from_unixtime(floor(CLIENT_TIME/1000),'yyyy-MM-dd') end as day,
+            APK_ID,count(*) as total 
+        from sdk200002
+        where VERSION_CODE<10000000000
+        group by case when CLIENT_TIME > SERVER_TIME*1000 or SERVER_TIME>(CLIENT_TIME/1000)+864000 then from_unixtime(SERVER_TIME,'yyyy-MM-dd') else from_unixtime(floor(CLIENT_TIME/1000),'yyyy-MM-dd') end,APK_ID;
+    
+    drop table market_game_download_error_stat;
+    create table if not exists market_game_download_error_stat (
+         day string,
+         time int,
+         apkid int,
+         total int
+     )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+    
+    insert overwrite table market_game_download_error_stat 
+     SELECT day,unix_timestamp(day,'yyyy-MM-dd')  as time,apkid,total 
+     FROM market_game_download_error_stat_tmp 
+     where total>0 and apkid<10000000000 and apkid>0
+     order by total desc;
+    
+    drop table market_game_download_error_stat_tmp;
+    
+    
+    
+    
+    create EXTERNAL table if not exists sdk200004 (
+        SERVER_TIME bigint,
+        CLIENT_IP string,
+        CLIENT_AREA string,
+        EVENT_CLASS_ID string,
+        CLIENT_TIME bigint,
+        EVENT_TAG string,
+        CELL_PHONE_BRAND string,
+        CELL_PHONE_MODEL string,
+        CELL_PHONE_CPU string,
+        CELL_PHONE_DENSITY string,
+        CELL_PHONE_SCREEN_WIDTH bigint,
+        CELL_PHONE_SCREEN_HEIGHT bigint,
+        CELL_PHONE_NETWORK string,
+        CELL_PHONE_SYSTEM_VERSION string,
+        MUZHIWAN_VERSION string,
+        CELL_PHONE_FIRMWARE string,
+        CELL_PHONE_DEVICE_ID string,
+        TITLE string,
+        PACKAGE_NAME string,
+        VERSION string,
+        VERSION_CODE bigint,
+        APK_LINK string,
+        APK_ID bigint,
+        APK_BAIDU_LINK string,
+        APK_SIZE bigint,
+        FLAG_GOOGLE_PLAY bigint,
+        FLAG_LOGIN_ACCOUNT bigint,
+        OPERATION_TAG string,
+        CHANNEL string,
+        APK_SOURCE string,
+        CATEGORY string,
+        COST_TIME bigint,
+        SERVER_IP string,
+        PROGRESS bigint,
+        REALURL string
+    )
+    row format delimited
+    fields terminated by '\\001'
+    stored as textfile
+    location '/apilogs/src/200004/';
+  
+    drop   table market_game_download_cancel_stat_tmp;
+    create table if not exists market_game_download_cancel_stat_tmp (
+        day string,
+        apkid int,
+        total int
+    )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+
+    insert overwrite table market_game_download_cancel_stat_tmp 
+        select case when CLIENT_TIME>SERVER_TIME*1000 or SERVER_TIME>(CLIENT_TIME/1000)+864000 then from_unixtime(SERVER_TIME,'yyyy-MM-dd') else from_unixtime(floor(CLIENT_TIME/1000),'yyyy-MM-dd') end as day,
+            APK_ID,count(*) as total 
+        from sdk200004 
+        where VERSION_CODE<10000000000
+        group by case when CLIENT_TIME > SERVER_TIME*1000 or SERVER_TIME>(CLIENT_TIME/1000)+864000 then from_unixtime(SERVER_TIME,'yyyy-MM-dd') else from_unixtime(floor(CLIENT_TIME/1000),'yyyy-MM-dd') end,APK_ID;
+    
+    drop table market_game_download_cancel_stat;
+    create table if not exists market_game_download_cancel_stat (
+         day string,
+         time int,
+         apkid int,
+         total int
+     )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+    
+    insert overwrite table market_game_download_cancel_stat 
+     SELECT day,unix_timestamp(day,'yyyy-MM-dd')  as time,apkid,total 
+     FROM market_game_download_cancel_stat_tmp 
+     where total>0 and apkid<10000000000 and apkid>0
+     order by total desc;
+    
+    drop table market_game_download_cancel_stat_tmp;
+    
+    
+    
+    drop table market_mobile_download_stat;
+    create table if not exists market_mobile_download_stat (
+         day string,
+         time int,
+         apkid int,
+         click int,
+         success int,
+         error int,
+         cancel int
+     )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+    
+    insert overwrite table market_mobile_download_stat 
+    SELECT a.*, b.total,c.total,d.total FROM market_game_download_click_stat a JOIN market_game_download_success_stat b ON (a.day = b.day and a.apkid=b.apkid)  JOIN market_game_download_error_stat c ON (c.day = b.day and c.apkid=b.apkid)  JOIN market_game_download_cancel_stat d ON (c.day = d.day and c.apkid=d.apkid) order by a.day desc;
+    
+    
+    
+    drop    table market_download_click_stat;
+    Create Table market_download_click_stat as  select a.day as day ,a.time as time,sum(a.total) as total from market_game_download_click_stat a group by a.day,a.time;
+    
+    drop    table market_download_success_stat;
+    Create Table market_download_success_stat as  select a.day as day ,a.time as time,sum(a.total) as total from market_game_download_success_stat a group by a.day,a.time;
+    
+    drop    table market_download_error_stat;
+    Create Table market_download_error_stat as  select a.day as day ,a.time as time,sum(a.total) as total from market_game_download_error_stat a group by a.day,a.time;
+    
+    drop    table market_download_cancel_stat;
+    Create Table market_download_cancel_stat as  select a.day as day ,a.time as time,sum(a.total) as total from market_game_download_cancel_stat a group by a.day,a.time;
+    
+    drop   table market_downlog_mob;
+    create table if not exists market_downlog_mob (
+         day string,
+         time int,
+         click int,
+         success int,
+         error int,
+         cancel int
+     )
+    Row Format Delimited
+    Fields Terminated By '\t'
+    stored as textfile;
+    
+     insert overwrite table market_downlog_mob 
+    SELECT a.*, b.total,c.total,d.total FROM market_download_click_stat a JOIN market_download_success_stat b ON (a.day = b.day)  JOIN market_download_error_stat c ON (c.day = b.day)  JOIN market_download_cancel_stat d ON (c.day = d.day) order by a.day desc;
+    
+    drop    table market_download_click_stat;
+    drop    table market_download_success_stat;
+    drop    table market_download_error_stat;
+    drop    table market_download_cancel_stat;
+    
+    
+ "
+ 
+ mysql -h10.1.1.16 -ustatsdkuser -pstatsdkuser2111579711 -D stat_sdk <<EOF
+
+    DROP TABLE IF EXISTS market_mobile_download_stat;
+    CREATE TABLE market_mobile_download_stat (
+          day varchar(255) NOT NULL,
+          time int(10) NOT NULL,
+          apkid int(10) NOT NULL,
+          click int(10) NOT NULL,
+          success int(10) NOT NULL,
+          error int(10) NOT NULL,
+          cancel int(10) NOT NULL,
+          KEY index_apkid (apkid),
+          KEY index_click (click),
+	      KEY index_success (success),
+	      KEY index_error (error),
+	      KEY index_cancel (cancel)
+          
+    );
+
+    DROP TABLE IF EXISTS market_downlog_mob;
+    CREATE TABLE market_downlog_mob (
+         day varchar(255) NOT NULL,
+         time int(10) NOT NULL,
+         click int(10) NOT NULL,
+         success int(10) NOT NULL,
+         error int(10) NOT NULL,
+         cancel int(10) NOT NULL
+    );
+    
+EOF
+
+ sudo -u hdfs  sqoop export --connect jdbc:mysql://10.1.1.16:3306/stat_sdk --username statsdkuser --password statsdkuser2111579711 --table market_mobile_download_stat --export-dir /user/hive/warehouse/market_mobile_download_stat --input-fields-terminated-by '\t' --input-null-string "\\\\N" --input-null-non-string "\\\\N"
+ sudo -u hdfs  sqoop export --connect jdbc:mysql://10.1.1.16:3306/stat_sdk --username statsdkuser --password statsdkuser2111579711 --table market_downlog_mob --export-dir /user/hive/warehouse/market_downlog_mob --input-fields-terminated-by '\t' --input-null-string "\\\\N" --input-null-non-string "\\\\N"
+
+mysql -h10.1.1.16 -ustatsdkuser -pstatsdkuser2111579711 -D stat_sdk -e "ALTER TABLE  market_mobile_download_stat  ADD id INT( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY   FIRST ;"
+mysql -h10.1.1.16 -ustatsdkuser -pstatsdkuser2111579711 -D stat_sdk -e "ALTER TABLE  market_downlog_mob  ADD id INT( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY   FIRST ;"
+
+ 
+ 
